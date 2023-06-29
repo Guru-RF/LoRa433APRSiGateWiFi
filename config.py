0ac04f589f71842878812bb1f151f5f6623f8c06
ure <ure@moreorless.be>
2023-06-30 01:33:48 +0200

    baseline for APRS iGate over WiFi

---
+++ config.py
@@ -0,0 +1,14 @@
+
+call = "ON3URE-5"
+aprs_host = "belgium.aprs2.net"
+aprs_port = 14580
+passcode = "23996" # https://apps.magicbug.co.uk/passcode/index.php/passcode
+latitude = 51.151965
+longitude = 2.765446
+altitude = 1
+comment = "https://rf.guru LoraAPRSGateway v0.1"
+symbol = "La"
+# http rest stats monitor
+enable = False
+token = "whatever"
+url = "http://api.lora.aprs.rf.guru"
